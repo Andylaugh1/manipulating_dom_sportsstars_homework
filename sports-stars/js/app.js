20113941cde2ac05dc5f9bd0f0583bd01b00cdad
Andy Laughlin <atl11384@gmail.com>
2018-06-22 13:15:30 +0100

    First part of app code written

---
+++ sports-stars/js/app.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const newSportstarForm = document.querySelector('#new-sportsman-form');
+  newSportstarForm.addEventListener('submit', handleNewSportstarFormSubmit);
+
+})
+
+const handleNewSportstarFormSubmit = function (event) {
+  event.preventDefault();
+
+const sportstar = createSportstar(event.target);
+const sportstarList = document.querySelector('#sportstar-list');
+sportstarList.appendChild(sportstar);
+
+event.target.reset();
+}
+
+const createSportstar = function (form) {
+  const sportstar = document.createElement('li');
+  sportstar.classList.add('sportstar');
+
+  const firstName = buildElement('h2', form.first-name.value);
+  sportstar.appendChild(firstName);
+  const lastName = buildElement('h3', form.last-name.value);
+  sportstar.appendChild(lastName);
+  const sport = buildElement('h3', form.sport.value);
+  sportstar.appendChild(sport);
+  const age = buildElement('h3', form.age.value);
+  sportstar.appendChild(age);
+}
